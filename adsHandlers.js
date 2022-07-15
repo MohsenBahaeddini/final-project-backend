a3f346eabff069e66be86f8774914d742dbc42b8
Mohsen Bahaeddini <mbahaeddinii@gmail.com>
2022-07-15 16:21:21 -0400

    backend

---
+++ adsHandlers.js
@@ -0,0 +1,350 @@
+// use this package to generate unique ids: https://www.npmjs.com/package/uuid
+const { v4: uuidv4 } = require("uuid");
+
+const { MongoClient, ObjectId } = require("mongodb");
+const { type } = require("os");
+const { query } = require("express");
+
+require("dotenv").config();
+
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// get all ads in db
+const getAds = async (req, res) => {
+  try {
+    const client = new MongoClient(MONGO_URI, options);
+    const page = req.query.page;
+    console.log(page);
+    const limit = 6;
+    await client.connect();
+    const db = client.db("mba");
+
+    const findAds = await db
+      .collection("ads")
+      .find()
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .toArray();
+    console.log(findAds);
+    const count = await db.collection("ads").countDocuments();
+    console.log(count);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      count,
+      ads: findAds,
+    });
+    client.close();
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  }
+  //  finally {
+  //   await client.close();
+  // }
+};
+
+// get a specific ad by id
+const getAd = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const adId = req.params.id;
+  // console.log(req);
+  try {
+    await client.connect();
+    const db = client.db("mba");
+    const findSingleAd = await db.collection("ads").findOne({ _id: adId });
+    res.status(200).json({
+      status: 200,
+      success: true,
+      ad: findSingleAd,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+// add new ad to db
+const addNewAd = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  const {
+    owner,
+    type,
+    make,
+    model,
+    year,
+    mileage,
+    description,
+    price,
+    imageUrl,
+    airConditioning,
+    alloyWheels,
+    bluetooth,
+    heatedSeats,
+    navigationSystem,
+    stabilityControl,
+  } = req.body;
+  const newAdId = uuidv4();
+  if (!type || !make || !year || !mileage || !model || !price) {
+    res.status(400).json({
+      status: 400,
+      error: "at least one info is missing",
+    });
+  }
+  try {
+    await client.connect();
+    const db = client.db("mba");
+
+    const newAd = {
+      owner: owner,
+      _id: newAdId,
+      type: type,
+      make: make,
+      year: year,
+      model: model,
+      mileage: mileage,
+      description: description,
+      price: price,
+      imageUrl: imageUrl,
+      airConditioning: airConditioning,
+      alloyWheels: alloyWheels,
+      bluetooth: bluetooth,
+      heatedSeats: heatedSeats,
+      navigationSystem: navigationSystem,
+      stabilityControl: stabilityControl,
+    };
+    const insertNewAd = await db.collection("ads").insertOne(newAd);
+    if (insertNewAd) {
+      res.status(200).json({
+        status: 200,
+        success: true,
+        newAd: newAd,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+// update an ad
+const updateAd = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  const { type, make, year, model, mileage } = req.body;
+
+  const adId = req.params.id;
+  const query = { _id: adId };
+  try {
+    await client.connect();
+    const db = client.db("mba");
+
+    const previousValues = await db.collection("ads").findOne(query);
+    console.log("previousValues : ", previousValues);
+
+    const newValues = {
+      $set: {
+        type: type || previousValues.type,
+        make: make || previousValues.make,
+        year: year || previousValues.year,
+        model: model || previousValues.model,
+        mileage: mileage || previousValues.mileage,
+      },
+    };
+    console.log(newValues);
+    const updateAd = await db.collection("ads").updateOne(query, newValues);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      updatedAd: updateAd,
+      modified: updateAd.modifiedCount,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+// delete an ad
+const deleteAd = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const adId = req.params.id;
+  try {
+    await client.connect();
+    const db = client.db("mba");
+
+    const ad = await db.collection("ads").deleteOne({ _id: adId });
+    res.status(200).json({
+      status: 200,
+      success: true,
+      deletedAd: ad,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+// get ads by owner
+const getAdsByOwners = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  const { id } = req.params;
+  try {
+    await client.connect();
+    const db = client.db("mba");
+
+    const findUser = await db.collection("users").findOne({ _id: id });
+
+    const findUserAds = await db
+      .collection("ads")
+      .find({ owner: findUser.email })
+      .toArray();
+    // console.log(findUserAds);
+
+    if (findUserAds) {
+      res.status(200).json({
+        status: 200,
+        success: true,
+        ads: findUserAds,
+      });
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: "this user does not have any ads",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+// add the ads that user saved
+const addSavedAd = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const { userId, adId } = req.body;
+  // do we need to get all feature of the car (ad)?
+
+  try {
+    await client.connect();
+    const db = client.db("mba");
+
+    const newSavedAd = { userId: userId, _id: adId };
+    const insertNewSavedAd = await db
+      .collection("savedAdsByUsers")
+      .insertOne(newSavedAd);
+    res.status(200).json({
+      status: 200,
+      success: true,
+      newSavedAd: newSavedAd,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+// get all saved ads by the specific user
+const getSavedAdsByUser = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  const userId = req.params.id;
+  try {
+    await client.connect();
+    const db = client.db("mba");
+    // find savedAds by userId in savedAdsByUsers collection
+    const findSavedAdsByUser = await db
+      .collection("savedAdsByUsers")
+      .find({ userId: userId })
+      .toArray();
+    console.log(findSavedAdsByUser);
+
+    if (findSavedAdsByUser) {
+      res.status(200).json({
+        status: 200,
+        success: true,
+        savedAds: findSavedAdsByUser,
+      });
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: "this user does not have any saved ads",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+// delete savedAd
+const deleteSavedAd = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const adId = req.params.id;
+  try {
+    await client.connect();
+    const db = client.db("mba");
+
+    const ad = await db.collection("savedAdsByUsers").deleteOne({ _id: adId });
+    res.status(200).json({
+      status: 200,
+      success: true,
+      deletedAd: ad,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      Error: err.message,
+    });
+  } finally {
+    await client.close();
+  }
+};
+
+module.exports = {
+  addNewAd,
+  getAds,
+  getAd,
+  deleteAd,
+  updateAd,
+  getAdsByOwners,
+  addSavedAd,
+  getSavedAdsByUser,
+  deleteSavedAd,
+};
---
+++ index.js
@@ -0,0 +1,100 @@
+"use strict";
+
+const exp = require("constants");
+// import the needed node_modules.
+const express = require("express");
+const morgan = require("morgan");
+
+const {
+  addNewAd,
+  getAds,
+  getAd,
+  deleteAd,
+  updateAd,
+  getAdsByOwners,
+  addSavedAd,
+  getSavedAdsByUser,
+  deleteSavedAd,
+} = require("./adsHandlers");
+
+const {
+  createConversation,
+  getConversationsByAdId,
+  updateConversation,
+  getConverationsById,
+  getConversationsByBuyerId,
+} = require("./ConversationsHandlers");
+
+const { addNewUser, getUsers, getUserById } = require("./usersHandlers");
+
+express()
+  // Below are methods that are included in express(). We chain them for convenience.
+  // --------------------------------------------------------------------------------
+
+  // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
+  .use(morgan("tiny"))
+  .use(express.json());
+if (process.env.NODE_ENV === "production") {
+  express().use(express.static("client/build"));
+}
+// Any requests for static files will go into the public folder
+express()
+  .use(express.static("public"))
+
+  // ---------------------------------
+  // REST endpoints
+
+  // get all ads
+  .get("/api/ads", getAds)
+  // get a specific ad
+  .get("/api/ad/:id", getAd)
+  // add new ad
+  .post("/api/new-ad", addNewAd)
+  // update ad
+  .patch("/api/update-ad/:id", updateAd)
+  // delete ad
+  .delete("/api/delete-ad/:id", deleteAd)
+  // add the ads that user has saved
+  .post("/api/new-saved-ad", addSavedAd)
+  // get savedAds by user
+  .get("/api/saved-ads-by-user/:id", getSavedAdsByUser)
+  // delete a specific savedAd
+  .delete("/api/delete-saved-ad/:id", deleteSavedAd)
+
+  // get all users
+  .get("/api/users", getUsers)
+  // add user
+  .post("/api/new-user", addNewUser)
+
+  // get ads by owner
+  .get("/api/ads-by-owner/:id", getAdsByOwners)
+
+  // get user by id
+  .get("/api/user/:id", getUserById)
+
+  // create new conversation
+  .post("/api/new-conversation", createConversation)
+
+  // get conversation by conversationId
+  .get("/api/conversation-by-id/:id", getConverationsById)
+
+  // get conversations for the specific ad
+  .get("/api/conversations-by-ad/:id", getConversationsByAdId)
+
+  // get conversations for buyers
+  .get("/api/conversations-by-buyers/:id", getConversationsByBuyerId)
+
+  // update conversation
+  .patch("/api/update-conversation/:id", updateConversation)
+  // ---------------------------------
+
+  // this is our catch all endpoint.
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not what you are looking for.",
+    });
+  })
+
+  // Node spins up the server and sets it to listen on port 8000.
+  .listen(process.env.PORT || 8001, console.log("listening on port 8001"));
